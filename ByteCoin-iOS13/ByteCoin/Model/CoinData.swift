c107e1ef22b3e88371b1951d557b78da948cd1b3
Shoeb0263 <103112482+Shoeb0263@users.noreply.github.com>
2023-12-19 18:03:26 +0530

    Add files via upload

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  SelectedCurrency.swift
+//  ByteCoin
+//
+//  Created by Shoeb's Macbook on 16/12/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct CoinData: Codable{
+    let rate: Double
+}
